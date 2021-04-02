7ca04a0eb352bc06089ee1bc8c92d06373bb6075
nguyen duc linh tuan <tuan2887903@gmail.com>
2021-04-02 11:18:45 +0700

    build prod

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = 9004;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
